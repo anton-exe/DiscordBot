27c22322253058a2c71c3855241a7940775cf2ba
Anton <antek@sniec.pl>
2022-01-29 15:14:37 +0000

    add a tests cog

--- main.py
+++ main.py
@@ -16,33 +16,35 @@ async def on_ready():
 async def logCommand(context):
 	print(f'\033[0;44m@{context.author}\033[0m ran \033[0;91m{context.prefix}{context.command}\033[0m in \033[0;44m#{context.channel.name}\033[0m on \033[1m{context.guild.name}\033[0m')
 
-@bot.command(name='server', description = 'Displays server info')
-async def fetchServerInfo(context):
-	guild = context.guild
-	
-	await context.send(f'Server Name: {guild.name}')
-	await context.send(f'Server Size: {len(guild.members)}')
-
-@bot.command(name='like', brief='Reaction Test', description = '"Likes" your command')
-async def like(context):
-	await context.message.add_reaction('👍')
-
 @bot.command(name='Troll')
 @commands.is_owner()
 async def atEveyone(context):
 	for i in range(100):
 		await context.send(f'@everyone #{i + 1}')
 
-@bot.command(name='poll')
+@bot.command(name='poll', brief="broken rn don't use")
 async def weLiveInADemocracy(context: commands.Context):
 	await context.send(context.message.content.split(" ", 2))
 
 @bot.command(name='shutdown')
 @commands.is_owner()
 async def shutdown(ctx):
-    await ctx.bot.close()
+	await ctx.bot.close()
 
-class Tests(commands.Cog()):
+class Tests(commands.Cog):
 	"""Just some tests"""
+
+	@commands.command(name='like', brief='Reaction Test', description = '"Likes" your command')
+	async def like(self, context):
+		await context.message.add_reaction('👍')
+
+	@commands.command(name='server', description = 'Displays server info')
+	async def fetchServerInfo(self, context):
+		guild = context.guild
+
+		await context.send(f'Server Name: {guild.name}')
+		await context.send(f'Server Size: {len(guild.members)}')
+
+bot.add_cog(Tests())
 
 bot.run(TOKEN) 
