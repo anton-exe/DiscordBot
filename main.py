e2df9420da6648c2f4d20198548db52160edfd10
anton-exe <antek@sniec.pl>
2022-01-29 17:24:50 +0000

    Make poll command

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='+')
 
+numberEmojis = [
+	"<a:zero:937030158085402664>",
+	"<a:one:937029141239308438>",
+	"<a:two:937029141239308438>",
+	"<a:three:937029141239308438>",
+	"<a:four:937029141239308438>",
+	"<a:five:937029141239308438>",
+	"<a:six:937029141239308438>",
+	"<a:seven:937029141239308438>",
+	"<a:eight:937029141239308438>",
+	"<a:nine:937029141239308438>",
+	"<a:keycap_ten:937031571507126312>"
+]
 @bot.event
 async def on_ready():
 	print(f'Bot connected as {bot.user}')
@@ -22,9 +35,16 @@ async def atEveyone(context):
 	for i in range(100):
 		await context.send(f'@everyone #{i + 1}')
 
-@bot.command(name='poll', brief="broken rn don't use")
-async def weLiveInADemocracy(context: commands.Context):
-	await context.send(context.message.content.split(" ", 2))
+@bot.command(name='poll', usage="question, [options]...", brief="broken rn don't use")
+async def weLiveInADemocracy(context):
+	cmdArgs = context.message.content.split(", ", 12)
+	question = cmdArgs[1]
+	choices = cmdArgs[2:]
+	desc = ""
+	for i in range(choices.length):
+		desc = desc + f'{numberEmojis[i]}: {choices[i]}/n'
+	embed = discord.Embed(title=question, description=desc)
+	msg = context.send(embed=embed)
 
 @bot.command(name='shutdown')
 @commands.is_owner()
