6665208f262cf055d21236ebbc44965ffa5ec197
Anton <antek@sniec.pl>
2022-01-29 18:07:51 +0000

    Add an "Anton_only" cog & change brief of poll

--- main.py
+++ main.py
@@ -44,13 +44,7 @@ async def on_ready():
 async def logCommand(context):
 	print(f'\033[0;44m@{context.author}\033[0m ran \033[0;91m{context.prefix}{context.command}\033[0m in \033[0;44m#{context.channel.name}\033[0m on \033[1m{context.guild.name}\033[0m')
 
-@bot.command(name='Troll')
-@commands.is_owner()
-async def atEveyone(context):
-	for i in range(100):
-		await context.send(f'@everyone #{i + 1}')
-
-@bot.command(name='poll', usage="question, [options]...", brief="broken rn don't use")
+@bot.command(name='poll', usage="question, [options]...", brief="auto generate a message to do a poll")
 async def weLiveInADemocracy(context):
 	cmdArgs = context.message.content.split(" ", 1)[1].split(", ", 22)
 	question = cmdArgs[0]
@@ -62,11 +56,6 @@ async def weLiveInADemocracy(context):
 	msg = await context.send(embed=embed)
 	for i in range(len(choices)):
 		await msg.add_reaction(letterEmojis[i])
-
-@bot.command(name='shutdown')
-@commands.is_owner()
-async def shutdown(context):
-	await context.bot.close()
 
 class Tests(commands.Cog):
 	"""Just some tests"""
@@ -82,6 +71,21 @@ class Tests(commands.Cog):
 		await context.send(f'Server Name: {guild.name}')
 		await context.send(f'Server Size: {len(guild.members)}')
 
+class Anton_only(commands.Cog):
+	"""Commands that can only be used by the creator of the bot, Anton"""
+
+	@commands.command(name='shutdown')
+	@commands.is_owner()
+	async def shutdown(self, context):
+		await context.bot.close()
+
+	@commands.command(name='Troll')
+	@commands.is_owner()
+	async def atEveyone(self, context):
+		for i in range(100):
+			await context.send(f'@everyone #{i + 1}')
+
 bot.add_cog(Tests())
+bot.add_cog(Anton_only())
 
 bot.run(TOKEN) 
